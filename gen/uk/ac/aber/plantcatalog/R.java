8eadccb640d1117802a41efbc4b4f0a863cc2886
anh45 <anh45@aber.ac.uk>
2015-01-22 15:49:23 +0000

    Added visits class and activities

--- gen/uk/ac/aber/plantcatalog/R.java
+++ gen/uk/ac/aber/plantcatalog/R.java
@@ -25,27 +25,39 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int userEmail=0x7f080002;
-        public static final int userLogOn=0x7f080003;
-        public static final int userName=0x7f080000;
-        public static final int userPhone=0x7f080001;
+        public static final int RelativeLayout1=0x7f080004;
+        public static final int action_settings=0x7f080008;
+        public static final int listView1=0x7f080005;
+        public static final int logOn_UserEmail=0x7f080002;
+        public static final int logOn_UserLogOn=0x7f080003;
+        public static final int logOn_UserName=0x7f080000;
+        public static final int logOn_UserPhone=0x7f080001;
+        public static final int visit_ReserveName=0x7f080006;
+        public static final int visit_StartRecording=0x7f080007;
     }
     public static final class layout {
         public static final int activity_log_on=0x7f030000;
-        public static final int activity_visit=0x7f030001;
+        public static final int activity_sightings_list=0x7f030001;
+        public static final int activity_visit=0x7f030002;
     }
     public static final class menu {
         public static final int log_on=0x7f070000;
-        public static final int visit=0x7f070001;
+        public static final int sightings_list=0x7f070001;
+        public static final int visit=0x7f070002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050003;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int log_on_button=0x7f050004;
-        public static final int title_activity_log_on=0x7f050001;
-        public static final int title_activity_visit=0x7f050005;
+        public static final int empty=0x7f050002;
+        public static final int log_on_button=0x7f050007;
+        public static final int reserveName_hint=0x7f050009;
+        public static final int startRecording=0x7f05000a;
+        public static final int title_activity_log_on=0x7f050003;
+        public static final int title_activity_sightings_list=0x7f05000b;
+        public static final int title_activity_visit=0x7f050008;
+        public static final int userEmail_hint=0x7f050006;
+        public static final int userName_hint=0x7f050004;
+        public static final int userPhone_hint=0x7f050005;
     }
     public static final class style {
         /** 
--- src/uk/ac/aber/rpsrrec/ui/SightingsListActivity.java
+++ src/uk/ac/aber/rpsrrec/ui/SightingsListActivity.java
@@ -1,40 +1,29 @@
 package uk.ac.aber.rpsrrec.ui;
 
-import java.text.DateFormat;
-import java.util.Date;
-
 import uk.ac.aber.plantcatalog.R;
-import uk.ac.aber.rpsrrec.data.User;
 import uk.ac.aber.rpsrrec.data.Visit;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
-public class VisitActivity extends Activity {
-
-	private Visit visit;
+public class SightingsListActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_visit);
+		setContentView(R.layout.activity_sightings_list);
 
 		Bundle data = getIntent().getExtras();
 
-		User user = data.getParcelable("user");
-		String date = DateFormat.getDateTimeInstance().format(new Date());
+		Visit visit = data.getParcelable("visit");
 
-		visit = new Visit(user, date);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.visit, menu);
+		getMenuInflater().inflate(R.menu.sightings_list, menu);
 		return true;
 	}
 
@@ -49,18 +38,4 @@ public class VisitActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/** Called when the user clicks the Start Recording button */
-	public void startRecording(View view) {
-		Intent intent = new Intent(this, SightingsListActivity.class);
-
-		EditText editText = (EditText) findViewById(R.id.visit_ReserveName);
-		String reserve = editText.getText().toString();
-
-		visit.setReserve(reserve);
-		intent.putExtra("visit", visit);
-
-		startActivity(intent);
-	}
-
 }
