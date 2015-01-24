a6107b6ec3dbb4727dbe3bec8441094e7503c616
Jon Shire <jos56@aber.ac.uk>
2015-01-24 19:03:13 +0000

    Re-added MessageBuilder in attempt to not have my work deleted this time.

--- src/uk/ac/aber/rpsrrec/http/MessageBuilder.java
+++ src/uk/ac/aber/rpsrrec/http/MessageBuilder.java
@@ -1,4 +1,4 @@
-package uk.ac.aber.rpserec.http;
+package uk.ac.aber.rpsrrec.http;
 
 import java.io.File;
 
--- src/uk/ac/aber/rpsrrec/http/MessageSender.java
+++ src/uk/ac/aber/rpsrrec/http/MessageSender.java
@@ -1,5 +1,30 @@
 package uk.ac.aber.rpsrrec.http;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 public class MessageSender {
 
+	private HttpClient defaultClient;
+	private HttpPost postServer;
+	
+	private MultipartEntityBuilder multipartMime;
+	
+	public MessageSender(MultipartEntityBuilder multipartMime){
+		this.multipartMime = multipartMime;
+		defaultClient = new DefaultHttpClient();
+		buildMessage();		
+	}
+	
+	public void buildMessage(){
+		postServer = new HttpPost("url");
+		postServer.setHeader("enctype", "multipart/mixed");
+		postServer.setEntity(multipartMime.build());
+	}
+	
+	public void sendMessage(){
+		
+	}
 }
